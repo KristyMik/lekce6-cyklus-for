0a6e147776aae9ddeb1fe79caaa977aba0e5e0f9
KristyMik <mikulcova.kristyna@gmail.com>
2022-03-22 19:28:33 +0100

    for cyklus implemented

--- main.js
+++ main.js
@@ -7,17 +7,25 @@ console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
 
+for(let i=0; i<11; i++){
+console.log(i);
 
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
+console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
+
+for(let i=0; i<=10; i++){
+    console.log(i);
+    
+    }
 
 console.log('-------------------');
 
@@ -26,6 +34,8 @@ console.log('-------------------');
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+for(let i=10; i>=0; i--){
+    console.log(i);
+    
+    }
 
-console.log('-------------------');
